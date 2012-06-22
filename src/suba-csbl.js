b0fc2e4bf7796aeeb92f590da3fcba4557807794
Leonardo Alberto Souza <leobetosouza@gmail.com>
2012-06-22 17:05:09 -0300

    init and grunt

---
+++ src/suba-csbl.js
@@ -0,0 +1,28 @@
+/*
+ * suba-csbl
+ * https://github.com/leobetosouza/suba-csbl
+ *
+ * Copyright (c) 2012 Leonardo Alberto Souza
+ * Licensed under the MIT license.
+ */
+
+(function($) {
+
+  // Collection method.
+  $.fn.awesome = function() {
+    return this.each(function() {
+      $(this).html('awesome');
+    });
+  };
+
+  // Static method.
+  $.awesome = function() {
+    return 'awesome';
+  };
+
+  // Custom selector.
+  $.expr[':'].awesome = function(elem) {
+    return elem.textContent.indexOf('awesome') >= 0;
+  };
+
+}(jQuery));
