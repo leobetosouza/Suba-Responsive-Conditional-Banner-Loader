76c7a970369d3054e8ff49976b94a65fdaf6cf2f
Leonardo Alberto Souza <leobetosouza@gmail.com>
2012-06-26 01:27:04 -0300

    converting tabs to spaces

--- src/old_code.js
+++ src/old_code.js
@@ -1,66 +1,66 @@
 (function(window, $) {
 
-	"use strict";
+  "use strict";
 
-	$(function() {
-		var sidebarBannerLoader = {};
+  $(function() {
+    var sidebarBannerLoader = {};
 
-		var $window = $(window);
-		var	$content = $('#content');
-		var	baseWidth = 1200;
-		var	bannerClass = 'contentExtend';
-		var	bannerLoaded = false;
-		var	isCallable = true;
-		var	hasBannerClass = $content.hasClass(bannerClass);
-		var	showBanners = function() {
-			if ($content.outerWidth() >= baseWidth) {
-				loadBanners();
-			} else {
-				removeBannerClass();
-			}
-		};
-		var	loadBanners = function() {
-			if (!bannerLoaded) {
-				//ajax: to call banners
-				
-				//success:
-				addBannerClass();
-				bannerLoaded = true;
+    var $window = $(window);
+    var $content = $('#content');
+    var baseWidth = 1200;
+    var bannerClass = 'contentExtend';
+    var bannerLoaded = false;
+    var isCallable = true;
+    var hasBannerClass = $content.hasClass(bannerClass);
+    var showBanners = function() {
+      if ($content.outerWidth() >= baseWidth) {
+        loadBanners();
+      } else {
+        removeBannerClass();
+      }
+    };
+    var loadBanners = function() {
+      if (!bannerLoaded) {
+        //ajax: to call banners
+        
+        //success:
+        addBannerClass();
+        bannerLoaded = true;
 
-				//var	$bSide = $content.find('.bannerSide');
-			
-				//fail:
-				//clearInterval(tInterval);
-				//$window.unbind('resize', isCallableFunc);
+        //var $bSide = $content.find('.bannerSide');
+      
+        //fail:
+        //clearInterval(tInterval);
+        //$window.unbind('resize', isCallableFunc);
 
-			} else {
-				addBannerClass();
-			}
-		};
-		var	isCallableFunc = function() {
-			isCallable = true;
-		};
-		var	addBannerClass = function() {
-			if (!hasBannerClass) {
-				$content.addClass(bannerClass);
-				hasBannerClass = true;
-			}
-		};
-		var	removeBannerClass = function() {
-			if (hasBannerClass) {
-				$content.removeClass(bannerClass);
-				hasBannerClass = false;
-			}
-		};
+      } else {
+        addBannerClass();
+      }
+    };
+    var isCallableFunc = function() {
+      isCallable = true;
+    };
+    var addBannerClass = function() {
+      if (!hasBannerClass) {
+        $content.addClass(bannerClass);
+        hasBannerClass = true;
+      }
+    };
+    var removeBannerClass = function() {
+      if (hasBannerClass) {
+        $content.removeClass(bannerClass);
+        hasBannerClass = false;
+      }
+    };
 
-		var	tInterval = setInterval(function() {
-			if (isCallable) {
-				showBanners();
-				isCallable = false;
-			}
-		}, 500);
+    var tInterval = setInterval(function() {
+      if (isCallable) {
+        showBanners();
+        isCallable = false;
+      }
+    }, 500);
 
-		$window.resize(isCallableFunc);
+    $window.resize(isCallableFunc);
 
-	});
+  });
 })(this, jQuery);
