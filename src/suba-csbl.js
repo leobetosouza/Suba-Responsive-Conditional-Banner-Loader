b34d281701186d23642ac6875cf50a1c03f24030
Leonardo Alberto Souza <leobetosouza@gmail.com>
2012-06-26 01:17:19 -0300

    initial tests

--- src/suba-csbl.js
+++ src/suba-csbl.js
@@ -1,28 +1,61 @@
 /*
- * suba-csbl
+ * suba-csbl * Conditional Sidebar Banners Loader
  * https://github.com/leobetosouza/suba-csbl
  *
  * Copyright (c) 2012 Leonardo Alberto Souza
  * Licensed under the MIT license.
  */
 
-(function($) {
+;(function(window, $) {
+
+  "use strict";
+
+  var pluginName = 'subaCSBL',
+    document = window.document,
+    defaults = {
+      bannerClass : 'contentExtend',
+      baseWidth : 1200
+    };
+
+  function BannerLoader( element, options ) {
+      this.element = element;
+      this.options = $.extend( {}, defaults, options) ;
+      
+      this._defaults = defaults;
+      this._name = pluginName;
+      
+      this.init();
+  }
+
+  BannerLoader.prototype.init = function () {
+    
+  };
+
+  if( typeof Object.create !== 'function' ) {
+    Object.create = function( o ) {
+      function F() {}
+      F.prototype = o;
+      return new F();
+    };
+  }
 
   // Collection method.
-  $.fn.awesome = function() {
-    return this.each(function() {
-      $(this).html('awesome');
-    });
+  $.fn[pluginName] = function ( options ) {
+      return this.each(function () {
+          if (!$.data(this, 'plugin_' + pluginName)) {
+              $.data(this, 'plugin_' + pluginName, new BannerLoader( this, options ));
+          }
+      });
   };
 
   // Static method.
-  $.awesome = function() {
-    return 'awesome';
-  };
+  // $.awesome = function() {
+  //   return 'awesome';
+  // };
 
   // Custom selector.
-  $.expr[':'].awesome = function(elem) {
-    return elem.textContent.indexOf('awesome') >= 0;
-  };
+  // $.expr[':'].awesome = function(elem) {
+  //   return elem.textContent.indexOf('awesome') >= 0;
+  // };
 
-}(jQuery));
+}(this, jQuery));
